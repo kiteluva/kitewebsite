1538bf6ed399dcca6ce44b3f20e345567cf1fb0c
James Kitenye <kitennykiluva@gmail.com>
2026-05-03 10:16:17 +0300

    well?????????

--- kiluvas/src/App.tsx
+++ kiluvas/src/App.tsx
@@ -196,12 +196,10 @@ const HomeView: React.FC<{ setActiveTab: (id: string) => void }> = ({ setActiveT
               In a community where only 1 in 20 children get the opportunity to step out, I’ve made it my mission to ensure that "success" isn't just personal—it's communal. I am driven to build the foundations and provide the resources that marginalized young minds need to venture into the tech world.
             </p>
             <p>
-              This vision led to the founding of 
-              <a href="https://the-website-chi.vercel.app" target="_blank" rel="noopener noreferrer" className="text-italic font-light hover:text-blue-400 transition-colors duration-200 flex items-center gap-1.5">
+              This vision led to the founding of <a href="https://the-website-chi.vercel.app" target="_blank" rel="noopener noreferrer" className="text-italic font-light hover:text-blue-400 transition-colors duration-200 flex items-center gap-1.5">
                 <img src="/images/icon2.png" alt="icon" className="w-4 h-4 inline-block" /> 
                 <strong>KADSA</strong>
-              </a> 
-              —a company dedicated to pushing the boundaries of Data, Tech, and AI. Through the KADSA Academy and our innovation pool, we provide quality knowledge to feed the ideas of innovators who, like me, come from backgrounds where amenities are scarce but potential is infinite.
+              </a> —a company dedicated to pushing the boundaries of Data, Tech, and AI. Through the KADSA Academy and our innovation pool, we provide quality knowledge to feed the ideas of innovators who, like me, come from backgrounds where amenities are scarce but potential is infinite.
             </p>
           </div>
           
